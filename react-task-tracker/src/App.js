4ce5f81af9c2fb2ebc405f2a0ed9959f37c815da
Amir9eng <amirmukhtarme@gmail.com>
2022-05-30 00:17:34 +0100

    addTask and toggle

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -5,26 +5,16 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask';
 
 function App () {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointments',
-      day: 'Feb 5th at 2:30pm ',
-      reminder: true
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm ',
-      reminder: true
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm ',
-      reminder: false
-    }
-  ])
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
+
+  }
 
   const deleteTask = id => {
     setTasks(tasks.filter(task => task.id !== id))
@@ -39,8 +29,8 @@ function App () {
   }
   return (
     <div className='container'>
-      <Header />{' '}
-      <AddTask />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}  />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
